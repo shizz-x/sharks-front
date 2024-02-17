9503538a2342beac7226fce2eb801c9e98d3e289
Jimmy Alfredo <jimmyalfredo@Jimmys-MacBook-Air.local>
2024-02-17 18:23:09 +0300

    fix video

--- src/components/VideoBanner/VideoBanner.jsx
+++ src/components/VideoBanner/VideoBanner.jsx
@@ -3,20 +3,24 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 
 VideoBanner.propTypes = {
-    video: PropTypes.string.isRequired,
-    borderRadius: PropTypes.bool,
-    sticky: PropTypes.bool
-}
+  video: PropTypes.string.isRequired,
+  borderRadius: PropTypes.bool,
+  sticky: PropTypes.bool,
+};
 
 export function VideoBanner(props) {
-    return (
-        <div className={classNames(styles.banner,props.borderRadius?styles.round:null,props.sticky?styles.sticky:null)}>
-            <video autoPlay muted loop id="myVideo">
-                <source src={props.video} type="video/mp4"/>
-            </video>
-            <div className={styles.containerVideo}>
-                {props.children}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className={classNames(
+        styles.banner,
+        props.borderRadius ? styles.round : null,
+        props.sticky ? styles.sticky : null
+      )}
+    >
+      <video controls={false} playsInline autoPlay muted loop id="myVideo">
+        <source src={props.video} type="video/mp4" />
+      </video>
+      <div className={styles.containerVideo}>{props.children}</div>
+    </div>
+  );
 }
